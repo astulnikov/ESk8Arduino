a3920146188b3de6eba5737858d7d278254dced1
alex.stulnikau <shamm.sb@gmail.com>
2019-05-20 09:55:15 +0200

    Make iut run

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,11 @@
 #include <Arduino.h>
+#include <VescUart.h>
+#include <SoftwareSerial.h>
+#include <Servo.h>
+
+const int STOP_ESC_VALUE = 1500;
+const int BACK_ESC_VALUE = 1200;
+const int RUN_ESC_VALUE = 1700;
 
 int fsrRearPin = 1;
 int fsrPin = 0;
@@ -8,33 +15,62 @@ int rearLEDpin = 10;
 int maxPressure = 0;
 int refPressure = 0;
 
+VescUart vesc;
+SoftwareSerial comSerial(2, 3); // RX, TX
+
 void setup() {
-  Serial.begin(9600);
+  comSerial.begin(9600);
+
   pinMode(LEDpin, OUTPUT);
   pinMode(rearLEDpin, OUTPUT);
+
+  Serial.begin(115200);
+  vesc.setSerialPort(&Serial);
 }
 
 void driveForward(int power){
+  comSerial.print("Power Forward = ");
+  comSerial.println(power);
+
   int ledBrightness = map(power, 0, 100, 0, 255);
   analogWrite(LEDpin, ledBrightness);
+  analogWrite(rearLEDpin, 0);
+
+  vesc.setRPM(100 * power);
 }
 
-void driveBackward(int power){
+void driveBackward(int power){ // Currently brakes
+  comSerial.print("Power Backward = ");
+  comSerial.println(power);
+
   int ledBrightness = map(power, 0, 100, 0, 255);
   analogWrite(rearLEDpin, ledBrightness);
+  analogWrite(LEDpin, 0);
+
+  vesc.setBrakeCurrent(power / 10);
+}
+
+void readVESC(){
+  if (vesc.getVescValues() ) {
+    comSerial.println(vesc.data.rpm);
+    comSerial.println(vesc.data.inpVoltage);
+    comSerial.println(vesc.data.ampHours);
+    comSerial.println(vesc.data.tachometerAbs);
+  }
 }
 
 void loop() {
+  readVESC();
   int fsrFrontReading = analogRead(fsrPin);
   int fsrRearReading = analogRead(fsrRearPin);
 
   int powerForward = 0;
   int powerBackward = 0;
   if(fsrFrontReading > 0 && fsrRearReading > 0){
-    Serial.print("fsrFrontReading = ");
-    Serial.println(fsrFrontReading);
-    Serial.print("fsrRearReading = ");
-    Serial.println(fsrRearReading);
+    comSerial.print("fsrFrontReading = ");
+    comSerial.println(fsrFrontReading);
+    comSerial.print("fsrRearReading = ");
+    comSerial.println(fsrRearReading);
 
     if(fsrFrontReading > maxPressure){
       maxPressure = fsrFrontReading;
@@ -54,16 +90,10 @@ void loop() {
       int frontPressure = map(fsrFrontReading, refPressure, maxPressure, 0, 100);
       int backPressure = map(fsrRearReading, refPressure, maxPressure, 0, 100);
 
-      // Serial.print("maxPressure = ");
-      // Serial.println(maxPressure);
-      // Serial.print("refPressure = ");
-      // Serial.println(refPressure);
-      // Serial.print("frontPressure = ");
-      // Serial.println(frontPressure);
-      // Serial.print("backPressure = ");
-      // Serial.println(backPressure);
+      frontPressure = max(0, frontPressure);
+      backPressure = max(0, backPressure);
 
-      int pressureThreshold = 15; // diff to move forward
+      int pressureThreshold = 25; // diff to move forward
       int backPressureThreshold = 40; // diff to apply breaks
       int maxPressure = 70; // the diff above means full throttle
 
@@ -75,27 +105,17 @@ void loop() {
         }
 
         driveForward(powerForward);
-        driveBackward(0);
       } else if(backPressure - frontPressure > backPressureThreshold){
         powerBackward = map(backPressure, backPressureThreshold, 100, 20, 100);
 
-        driveForward(0);
         driveBackward(powerBackward);
       } else {
         driveForward(0);
-        driveBackward(0);
       }
     }
   } else{
     driveForward(0);
-    driveBackward(0);
   }
-
-  Serial.print("Power Forward = ");
-  Serial.println(powerForward);
-  Serial.print("Power Backward = ");
-  Serial.println(powerBackward);
-  Serial.println("------------------");
 
   delay(200);
 }
